7c850cdd269c9a4bc5f4f03a2d653e548b4a7ec8
karaoku <58955178+karaoku@users.noreply.github.com>
2020-05-30 13:54:43 +0300

    AracConverter eklendi

---
+++ src/java/Converter/AracConverter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Converter;
+
+import dao.AracDAO;
+import entity.Arac;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+
+@FacesConverter("AracConverter")
+public class AracConverter implements Converter{
+
+    private AracDAO aracDAO;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return this.getAracDAO().find(Long.valueOf(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object arg2) {
+        Arac t = (Arac) arg2;
+        return t.getAracid().toString();
+    }
+
+    public AracDAO getAracDAO() {
+        if (this.aracDAO == null) {
+            aracDAO = new AracDAO();
+        }
+        return aracDAO;
+    }
+
+    public void setAracDAO(AracDAO aracDAO) {
+        this.aracDAO = aracDAO;
+    }
+
+}
