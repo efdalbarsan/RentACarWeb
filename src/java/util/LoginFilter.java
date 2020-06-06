316cff7604a3827cef95b234218ba32e5ec7fc79
EFDAL AKIN BARSAN <61588170+efdalbarsan@users.noreply.github.com>
2020-06-06 23:17:54 +0300

    Projenin tamamlanması için gerekli işlemler

--- src/java/util/LoginFilter.java
+++ src/java/util/LoginFilter.java
@@ -9,6 +9,7 @@ import entity.Kullanici;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -49,5 +50,15 @@ public class LoginFilter implements Filter {
             }
         }
 
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
